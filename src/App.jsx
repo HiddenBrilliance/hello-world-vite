92fa00ecc689686467cedba99a6115adfb849071
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2023-09-25 22:04:57 -0500

    added fight number search

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { flightsInfoArray } from './flight-info'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { flights } from "./flightsMockData";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredResults = () => {
+    return flights.filter((flight) =>
+      flight.flight_number.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  };
+
+  const renderDate = (date) => {
+    // TODO: figure out why this method is failing
+    return Intl.DateTimeFormat(date);
+  };
 
   return (
     <>
-<header>    
+      <header>
+        <input
+          placeholder="Enter Flight #"
+          onChange={(event) => setSearchQuery(event.target.value)}
+        />
+      </header>
 
-<div className='header-info'>
-      <h1>Travel Tracker</h1>
-<form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-</div>
-</header>  
+      <div className="table">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Flight Number</th>
+              <th scope="col">Departure Airport</th>
+              <th scope="col">Arrival Airport</th>
+              <th scope="col">Departure Time</th>
+              <th scope="col">Arrival Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Should display flight number with brand first i.e DL7077 */}
+            {filteredResults().map(function (flight) {
+              return (
+                //row
+                <tr>
+                  <td>{flight.flight_number}</td>
+                  <td>{flight.departure_airport}</td>
+                  <td>{flight.arrival_airport}</td>
+                  <td>{flight.departure_time}</td>
+                  <td>{renderDate(flight.arrival_time)}</td>
+                </tr>
+              );
+            })}{" "}
+          </tbody>
+        </table>
+      </div>
 
-
-<div className='table'> 
-<table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Flight Number</th>
-      <th scope="col">Departure Airport</th>
-      <th scope="col">Arrival Airport</th>
-      <th scope="col">Departure Time</th>
-      <th scope="col">Arrival Time</th>
-
-    </tr>
-  </thead>
-  <tbody>
-    
-    <tr>
-      {/* Should display flight number with brand first i.e DL7077 */}
-      <th scope="row">
-      
-       <td> 
-        
-        {flightsInfoArray.map(function(flight) {
-      return (
-        <div>
-          {flight.flight_number}
-        </div>
-      );
-    })} </td>
-
-
-    
-    </th>
-      {/* Departure Airport */}
-      <td> 
-        
-        {flightsInfoArray.map(function(flight) {
-      return (
-        <div>
-          {flight.departure_airport}
-        </div>
-      );
-    })} </td>
-    {/* Arrival Airport */}
-    <td> 
-        
-        {flightsInfoArray.map(function(flight) {
-      return (
-        <div>
-          {flight.arrival_airport}
-        </div>
-      );
-    })} </td>
-    {/* Departure Time */}
-    <td> 
-        
-        {flightsInfoArray.map(function(flight) {
-      return (
-        <div>
-          {flight.departure_time}
-        </div>
-      );
-    })} </td>
-    {/* Arrival Time */}
-    <td> 
-        
-        {flightsInfoArray.map(function(flight) {
-      return (
-        <div>
-          {flight.arrival_time}
-        </div>
-      );
-    })} </td>
-    </tr>
-  
-   
-  </tbody>
-</table>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* Not sure what to do with this yet */}
+      {/* Not sure what to do with this yet */}
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -124,7 +69,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
